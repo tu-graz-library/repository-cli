8abb81a2d15f0246678d6d96c93ba4d113a428c0
rekt-hard <72449192+rekt-hard@users.noreply.github.com>
2021-03-30 13:20:25 +0200

    Fix min version of invenio_rdm_records
    Co-authored-by: Mojib Wali <44528277+mb-wali@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ install_requires = [
     "click>=7.1.1,<8.0",
     "invenio-access>=1.4.2",
     "invenio-accounts>=1.4.0",
-    "invenio_rdm_records>0.28.0,<0.29.0",
+    "invenio_rdm_records>=0.28.0,<0.29.0",
 ]
 
 packages = find_packages()
